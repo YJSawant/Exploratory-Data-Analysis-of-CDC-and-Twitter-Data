3c29fcb0ce8c69cc658ed77cf9772f25ea136c6a
YJSawant <37597771+YJSawant@users.noreply.github.com>
2019-03-12 11:51:57 -0400

    Update Flu_Influenza_twitter.R

--- part3/task5/Flu_Influenza_twitter.R
+++ part3/task5/Flu_Influenza_twitter.R
@@ -10,10 +10,10 @@ library(dplyr)
 register_google(key = 'AIzaSyAqU3dQ5RUHNQJ5Tnl0Odyp1bk2Jiu6bmA') 
 create_token(
   app = "DIC lab 1",
-  consumer_key = "jK2bsMkRHNXthROyrplDieZaV",
-  consumer_secret = "4mjuIGQWkBtZJVq1ffyN4b9GCty6ChDRJxdUBFzxRUyqzHPOUw",
-  access_token = "128911319-6epULaDIDCVxrjPxB29LHPvOH0vnhnvvdscGRH9n",
-  access_secret = "xnZvtP6ZUaMDNoVeCiUSmXDL9fv7cqZqipoUdzPcc54L0")
+  consumer_key = "",
+  consumer_secret = "",
+  access_token = "",
+  access_secret = "")
 rt <- search_tweets("influenza", geocode = lookup_coords("usa"), n = 3000,include_rts = FALSE)
 rtflu<- search_tweets("flu", geocode = lookup_coords("usa"), n = 3000,include_rts = FALSE)
 rtboth<-rbind(rt,rtflu)
